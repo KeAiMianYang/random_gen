8eb2b4a4c244d834984de0f8745420789c12ba88
khorgn <hugo.dupuyroudel@laposte.net>
2016-02-01 23:32:57 +0100

    aa

--- v2_production/PerlinNoise.cpp
+++ v2_production/PerlinNoise.cpp
@@ -4,21 +4,21 @@
 #include "Generator.hpp"
 #include "Strategy.hpp"
 
-PlainArray* PerlinNoise::generate_PerlinNoise(Strategy strategy, int min, int max, int dimension, int* sizes){ // needs to include a library
+PlainArray* PerlinNoise::generate_perlinNoise(Strategy strategy, int min, int max, int dimension, int* sizes){ // needs to include a library
   PlainArray* p = new PlainArray(sizes, dimension);
   return p;
 }
 
 int* PerlinNoise::generate_perlinNoise_1D(Strategy strategy, int min, int max, int size){
-  PlainArray* p = generate_perlinNoise(strategy, min, max, 1, &size);
+  PlainArray* p = PerlinNoise::generate_perlinNoise(strategy, min, max, 1, &size);
 }
 int** PerlinNoise::generate_perlinNoise_2D(Strategy strategy, int min, int max, int sizeX, int sizeY){
   int sizes[] = {sizeX, sizeY};
-  PlainArray* p = generate_perlinNoise(strategy, min, max, 2, sizes);
+  PlainArray* p = PerlinNoise::generate_perlinNoise(strategy, min, max, 2, sizes);
 }
 int*** PerlinNoise::generate_perlinNoise_3D(Strategy strategy, int min, int max, int sizeX, int sizeY, int sizeZ){
   int sizes[] = {sizeX, sizeY, sizeZ};
-  PlainArray* p = generate_perlinNoise(strategy, min, max, 3, sizes);
+  PlainArray* p = PerlinNoise::generate_perlinNoise(strategy, min, max, 3, sizes);
 }
 
 int* PerlinNoise::convert_one_dim(PlainArray plainArray){
--- v2_production/PerlinNoise.hpp
+++ v2_production/PerlinNoise.hpp
@@ -8,7 +8,7 @@ class PlainArray;
 
 class PerlinNoise{
 public:
-static PlainArray* generate_PerlinNoise(Strategy strategy, int min, int max, int dimension, int* sizes);
+static PlainArray* generate_perlinNoise(Strategy strategy, int min, int max, int dimension, int* sizes);
 static int* generate_perlinNoise_1D(Strategy strategy, int min, int max, int size);
 static int** generate_perlinNoise_2D(Strategy strategy, int min, int max, int sizeX, int sizeY);
 static int*** generate_perlinNoise_3D(Strategy strategy, int min, int max, int sizeX, int sizeY, int sizeZ);
@@ -16,6 +16,10 @@ private:
 static int* convert_one_dim(PlainArray plainArray);
 static int** convert_two_dim(PlainArray plainArray);
 static int*** convert_three_dim(PlainArray plainArray);
+};
+
+class Aaaa{
+
 };
 
 #endif
